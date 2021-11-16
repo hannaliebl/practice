1a56dfebc6e87675aaa1adcbae2f139edaa48e7e
Hanna Liebl <hannaliebl@users.noreply.github.com>
2021-11-15 22:32:58 -0500

    Add two sum II solution

---
+++ leetcode/167-two-sum-ii.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// easy rating
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[left] + numbers[right] === target) {
+      return [left + 1, right + 1];
+    } else if (numbers[left] + numbers[right] < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+};
