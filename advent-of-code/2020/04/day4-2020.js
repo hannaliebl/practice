b0f18462e43096cc0798a90a61fe3937c90bc2fd
Hanna Liebl <hannaliebl@users.noreply.github.com>
2021-12-01 22:42:55 -0500

    Correct day 4 part 1 of 2020

--- advent-of-code/2020/04/day4-2020.js
+++ advent-of-code/2020/04/day4-2020.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 
+// part 1
 const inputData = fs
   .readFileSync("input.txt", "utf8")
   .split(/\n\n/)
@@ -14,53 +15,24 @@ const inputData = fs
       }, {});
   });
 
-// reduce((accum, curr) => {
-
-// }, {})
-// .reduce((accum, curr) => {
-//   if (curr.length > 0) {
-//     accum.push(curr);
-//   }
-// }, []);
 console.log(inputData);
 
-// function getInput() {
-//   return fs
-//     .readFileSync("input.txt")
-//     .toString()
-//     .split("\n\n")
-//     .map((item) =>
-//       item
-//         .replace(/\n/g, " ")
-//         .split(" ")
-//         .reduce((accum, field) => {
-//           const kv = field.split(":");
-//           accum[kv[0]] = kv[1];
-//           return accum;
-//         }, {})
-//     );
-// }
+function processInputData(data) {
+  let validCount = 0;
+  data.forEach((passportObj) => {
+    if (
+      passportObj.hasOwnProperty("ecl") &&
+      passportObj.hasOwnProperty("pid") &&
+      passportObj.hasOwnProperty("eyr") &&
+      passportObj.hasOwnProperty("hcl") &&
+      passportObj.hasOwnProperty("byr") &&
+      passportObj.hasOwnProperty("iyr") &&
+      passportObj.hasOwnProperty("hgt")
+    ) {
+      validCount++;
+    }
+  });
+  console.log("valid password count", validCount);
+}
 
-const test = [
-  "ecl:grn",
-  "cid:315 iyr:2012 hgt:192cm eyr:2023 pid:873355140 byr:1925 hcl:#cb2c03",
-  "",
-  "byr:2027 hcl:ec0cfd ecl:blu cid:120",
-  "eyr:1937 pid:106018766 iyr:2010 hgt:154cm",
-  "",
-];
-
-const foo = test.reduce((accum, curr, i, arr) => {
-  let store = {};
-  if (curr.length > 0) {
-    store.data = curr;
-    accum.push(store);
-    return accum;
-  } else {
-    return accum;
-  }
-}, []);
-
-console.log(foo);
-
-// console.log(getInput());
+processInputData(inputData);
