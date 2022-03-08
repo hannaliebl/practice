80c5f381d08969513861a6217cce70fd0dd6b5a1
Hanna Liebl <hannaliebl@users.noreply.github.com>
2022-03-07 21:17:03 -0500

    Add fruit into baskets solution

---
+++ leetcode/209-fruit-into-baskets.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/fruit-into-baskets/
+// medium
+
+// Map is likely cleaner
+// Tried Set first, but it doesn't work when input has multiple of same in row, like 3, 3, 1, 2, 1 as it doesn't keep
+// track of count. There is a clever solution with set, though: https://leetcode.com/problems/fruit-into-baskets/discuss/1230969/JS-with-Set-O(N)
+var totalFruit = function (fruits) {
+  let maxFruits = 0;
+  let left = 0;
+  let currentFruits = {};
+  for (let right = 0; right < fruits.length; right++) {
+    while (
+      Object.keys(currentFruits).length === 2 &&
+      currentFruits[fruits[right]] === undefined
+    ) {
+      currentFruits[fruits[left]]--;
+      if (currentFruits[fruits[left]] === 0) delete currentFruits[fruits[left]];
+      left++;
+    }
+    currentFruits[fruits[right]]
+      ? currentFruits[fruits[right]]++
+      : (currentFruits[fruits[right]] = 1);
+    maxFruits = Math.max(maxFruits, right - left + 1);
+  }
+  return maxFruits;
+};
+
+console.log(totalFruit([3, 3, 1, 2, 1])); // 3
+console.log(totalFruit([1, 2, 1])); // 3
+console.log(totalFruit([0, 1, 2, 2])); // 3
+console.log(totalFruit([1, 2, 3, 2, 2])); // 4
